8f5849e09392ea2fc0a240293a61fdb45a40b513
xxkl1 <sanyuelihuaxue@gmail.com>
2025-12-14 10:35:36 +0800

    feat: 完善单元测试ensure

--- test/ensure.ts
+++ test/ensure.ts
@@ -1,5 +1,5 @@
 const ensure = function (result: any, expect: any, message: string) {
-    if (JSON.stringify(result) === JSON.stringify(expect)) {
+    if (typeof result === typeof expect && JSON.stringify(result) === JSON.stringify(expect)) {
         console.log(`${message} pass`);
     } else {
         console.error(`${message} fail, expect: ${JSON.stringify(expect)}, actual: ${JSON.stringify(result)}`);
