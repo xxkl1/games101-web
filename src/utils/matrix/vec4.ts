c677d8296f0dee17b6dbcadb162e52a18832f45d
xxkl1 <sanyuelihuaxue@gmail.com>
2026-01-06 23:09:30 +0800

    feat: add todo

--- src/utils/matrix/vec4.ts
+++ src/utils/matrix/vec4.ts
@@ -1,4 +1,5 @@
 // 4维列向量
+// TODO：Vec4、Vec3和Mat4去掉类和范型的使用，统一改成对象的形式，例如 Vec4 = {x: number, y: number, z: number, w: number}
 class Vec4 {
     value: number[] = [0, 0, 0, 1,];
     constructor(value: number[]) {
