3b8d55997450939a3fb1612e2fad3404def35ddb
xxkl1 <sanyuelihuaxue@gmail.com>
2026-01-06 23:04:45 +0800

    feat: 添加向量初始化维度检测

--- src/utils/matrix/vec3.ts
+++ src/utils/matrix/vec3.ts
@@ -2,6 +2,7 @@
 class Vec3 {
     value: number[] = [0, 0, 0];
     constructor(value: number[]) {
+        if (value.length !== 3) throw new Error("value length must be 3");
         this.value = value;
     }
 }
--- src/utils/matrix/vec4.ts
+++ src/utils/matrix/vec4.ts
@@ -2,6 +2,7 @@
 class Vec4 {
     value: number[] = [0, 0, 0, 1,];
     constructor(value: number[]) {
+        if (value.length !== 4) throw new Error("value length must be 4");
         this.value = value;
     }
 }
