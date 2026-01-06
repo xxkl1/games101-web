3eafb61a069a665da004ade8a3547b3aa48f3c11
xxkl1 <sanyuelihuaxue@gmail.com>
2026-01-06 20:59:26 +0800

    refactor: 抽离点计算

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { vec4ToPoint } from "./utils/common";
 import { drawLine } from "./utils/draw/line";
-import { multiply, ndc2Screen, projection, rotate, Vec4, viewMat } from "./utils/matrix";
+import { Mat4, multiply, ndc2Screen, projection, rotate, Vec4, viewMat } from "./utils/matrix";
 import { perspectiveDivide } from "./utils/matrix/perspectiveDivide";
+
+const calcPoint = function (vec4: Vec4, matMVP: Mat4, matNdc2Screen: Mat4) {
+  const afterMVP = multiply(matMVP, vec4);
+  const vec = multiply(matNdc2Screen, perspectiveDivide(afterMVP));
+  return vec4ToPoint(vec);
+}
 
 const main = function () {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -16,9 +22,9 @@ const main = function () {
   const ndc2ScreenMat = ndc2Screen(w, h);
 
   const imageData = new ImageData(w, h);
-  const p1 = vec4ToPoint(multiply(ndc2ScreenMat, perspectiveDivide(multiply(mvp, new Vec4([0, 5, -5, 1])))));
-  const p2 = vec4ToPoint(multiply(ndc2ScreenMat, perspectiveDivide(multiply(mvp, new Vec4([5, 0, -5, 1])))));
-  const p3 = vec4ToPoint(multiply(ndc2ScreenMat, perspectiveDivide(multiply(mvp, new Vec4([-5, -5, -5, 1])))));
+  const p1 = calcPoint(new Vec4([0, 5, -5, 1]), mvp, ndc2ScreenMat);
+  const p2 = calcPoint(new Vec4([5, 0, -5, 1]), mvp, ndc2ScreenMat);
+  const p3 = calcPoint(new Vec4([-5, -5, -5, 1]), mvp, ndc2ScreenMat);
 
   drawLine(imageData, p1, p2, { r: 255, g: 0, b: 0, a: 255 });
   drawLine(imageData, p2, p3, { r: 0, g: 255, b: 0, a: 255 });
